dfb87d3c052caf6a9dbc6d8a790ae919abb27a10
André Brás <andrebras@gmail.com>
2024-02-29 13:25:47 +0000

    Add fish config

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,33 @@
+# Assuming the 'alias' directory is in the same directory as config.fish
+set config_dir (string join "" (dirname (status --current-filename)) "/conf.d")
+
+# Editor
+set -x EDITOR 'vim'
+set -x VISUAL 'vim'
+set -x PAGER 'less'
+
+# Locale
+# set -x LANG "en_US.UTF-8"
+# set -x LC_ALL "en_US.UTF-8"
+
+# Environment variables
+source $config_dir/env.fish
+source $config_dir/profile.fish
+
+# asdf
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# Set the list of directories that Fish shell searches for programs
+set -U path /usr/local/bin /usr/local/sbin $path
+
+if status is-interactive
+    # Interactive session specific commands go here
+    # (e.g., setting prompt, welcome message, alias etc.)
+    load_alias "$config_dir/alias"
+end
+
+# Homebrew
+eval (/opt/homebrew/bin/brew shellenv)
+
+# Hook direnv into Fish shell
+eval (direnv hook fish)
